c1aa8a5b3a57beaca7182ab5a4b0f7846e6d883f
camilo23401 <ca.garciav@uniandes.edu.co>
2021-04-08 19:21:36 -0500

    Avance minúsculo

---
+++ Caso2/src/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+
+	private int numMarcos;
+	private int numPaginas;
+	private int nivLocalidad;
+	
+	private int[] ramFake;
+	private int[] tablaPags;
+	private int[] edad;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		tablaPags =
+		
+
+	}
+
+}
