811004671cfb6daab66e620bf3316854d56c94e8
notegac <n.ortegac@uniandes.edu.co>
2021-04-09 19:19:16 -0500

    No sé qué pasa :(

--- Caso2/src/PrimerThread.java
+++ Caso2/src/PrimerThread.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
 
-public class PrimerThread extends Thread{
-	
+public class PrimerThread extends Thread {
+
 	private int[] ramFake;
 	private int[] tablaPags;
-	private int[] secuencia;
+	private ArrayList<Integer> secuencia;
 	private int[] edad;
 	private ArrayList<Integer> buffer;
 	private int numMarcos;
 
 	private int fallosPag;
-	
-	public PrimerThread(int[] pSecuencia, int[] pRamFake, int[] pTablaPags,  int[] edad, ArrayList<Integer> buffer, int numMarcos)
-	{
+
+	public PrimerThread(ArrayList<Integer> pSecuencia, int[] pRamFake, int[] pTablaPags, int[] edad,
+			ArrayList<Integer> buffer, int numMarcos) {
 		ramFake = pRamFake;
 		tablaPags = pTablaPags;
 		secuencia = pSecuencia;
@@ -21,55 +21,50 @@ public class PrimerThread extends Thread{
 		this.buffer = buffer;
 		this.numMarcos = numMarcos;
 	}
-	
-	public void run()
-	{
+
+	public void run() {
 		int cargadoARam = 0;
-		for(int i=0;i<secuencia.length;i++)
-		{
-			try
-			{
-				int actual = secuencia[i];
-				synchronized (ramFake){
+		for (int i = 0; i < secuencia.size(); i++) {
+			try {
+				int actual = secuencia.get(i);
+
+				synchronized (ramFake) {
 					buffer.add(actual);
-					if(cargadoARam<numMarcos)
-					{
+					if (cargadoARam < numMarcos) {
 						fallosPag++;
 						ramFake[cargadoARam] = actual;
+						edad[cargadoARam] = 0;
 						tablaPags[actual] = cargadoARam;
 						cargadoARam++;
 					}
-					//Revisar si la pagina est� en "ram" con la tabla de p�ginas
-					else if (tablaPags[actual] == -1){
-						//Si no est�, fallo de pag
+					// Revisar si la pagina est� en "ram" con la tabla de p�ginas
+					else if (tablaPags[actual] == -1) { // Si no est�, fallo de pag
 						fallosPag++;
-						//Reemplazar por la pag de la ram menos llamada
+						// Reemplazar por la pag de la ram menos llamada
 						int index = indicePagMenosUsada();
 						tablaPags[ramFake[index]] = -1;
 						ramFake[index] = actual;
+						edad[index] = 0;
 						tablaPags[actual] = index;
 					}
 				}
 
 				Thread.sleep(5);
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		System.out.println(fallosPag);
+		System.out.println("Número total de fallos de página: " + fallosPag);
 	}
 
-	private int indicePagMenosUsada(){
-		synchronized (edad){
-			int index = 0;
-			for (int i=1; i<edad.length; i++){
-				if (edad[i]<edad[index])
+	private int indicePagMenosUsada() {
+		int index = 0;
+		for (int i = 1; i < edad.length; i++) {
+			if (edad[i] < edad[index])
 				index = i;
-			}
-			return index;
 		}
+		return index;
+
 	}
 
 }
--- Caso2/src/Principal.java
+++ Caso2/src/Principal.java
@@ -1,33 +1,36 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.stream.Collector;
 
 public class Principal {
 
 	private static int numMarcos;
 	private static int numPaginas;
-	private static int nivLocalidad;
 	
-	private static int[] ramFake = {0,0,0,0,0,0,0,0};
-	private static int[] edad = {0,0,0,0,0,0,0,0};	//mismo tamaño que la ram
+	private static int[] ramFake;
+	private static int[] edad;	//mismo tamaño que la ram
 	private static int[] tablaPags;
-	private static int[] secuencia;
+	private static ArrayList<Integer> secuencia;
 	
 	public static void main(String[] args) {
 		try {
-			FileReader reader = new FileReader("data/referencias3.txt");
+			FileReader reader = new FileReader("Caso2/data/referencias4_8.txt");
 			BufferedReader br =  new BufferedReader(reader); 
 
 			numMarcos = Integer.parseInt(br.readLine());
 			numPaginas = Integer.parseInt(br.readLine());
+
+			ramFake = new int[numMarcos];
+			edad = new int[numMarcos];
+			ArrayList<Integer> buffer = new ArrayList<Integer>();
+
 			br.readLine();
 
-			String linea = "";
-			secuencia =  new int[1000];
+			String linea;
+			secuencia =  new ArrayList<Integer>();
 			int i = 0;
 			while ((linea = br.readLine()) != null){
-				secuencia[i++] = Integer.parseInt(linea);
+				secuencia.add(Integer.parseInt(linea));
 			}
 			br.close();
 
@@ -35,12 +38,6 @@ public class Principal {
 			for (i=0; i<numPaginas; i++){
 				tablaPags[i] = -1;
 			}
-			
-			
-			/* numMarcos = 8;
-			numPaginas = 16; */
-			
-			ArrayList<Integer> buffer = new ArrayList<Integer>();
 			
 			PrimerThread primero = new PrimerThread(secuencia, ramFake, tablaPags, edad, buffer, numMarcos);
 			SegundoThread segundo = new SegundoThread(ramFake, edad, buffer);
